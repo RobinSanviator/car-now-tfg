be2adb814085f778a891f328645ff192cc511872
RobinSanviator <131857498+RobinSanviator@users.noreply.github.com>
2025-03-12 00:23:46 +0100

    Creando primeras pantallas

--- app/src/main/java/com/example/carnowapp/vista/activity/LoginActivity.java
+++ app/src/main/java/com/example/carnowapp/vista/activity/LoginActivity.java
@@ -1,24 +1,26 @@
-package com.example.carnowapp.vista;
+package com.example.carnowapp.vista.activity;
 
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import com.airbnb.lottie.LottieAnimationView;
 import com.example.carnowapp.R;
 import com.google.android.material.button.MaterialButton;
 
-public class ActividadIntroduccion extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     private MaterialButton botonEmpezar;
-    private LottieAnimationView vistaAnimacion;
     private ImageView ivLogo;
-    private LinearLayout llBotones;
+    private LinearLayout linearBotones;
+    private MaterialButton botonInicio;
 
 
     @Override
@@ -26,7 +28,7 @@ public class ActividadIntroduccion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         EdgeToEdge.enable(this);
-        setContentView(R.layout.actividad_introduccion);
+        setContentView(R.layout.actividad_register);
 
         iniciarAnimacion();
 
@@ -34,20 +36,23 @@ public class ActividadIntroduccion extends AppCompatActivity {
 
     private void iniciarAnimacion() {
         ivLogo = findViewById(R.id.iv_logo);
+        botonInicio = findViewById(R.id.mb_iniciar_sesion);
         botonEmpezar = findViewById(R.id.mb_empezar);
-        llBotones = findViewById(R.id.linear_acceso);
+        linearBotones= findViewById(R.id.linear_acceso);
 
         if (ivLogo != null) {
             animacionLogo(ivLogo);
         } else {
-            Log.e("ActividadIntroduccion", "ivLogo es null.");
+            Log.e("ActividadIntroduccion", "ivLogo es null");
         }
 
-        if (llBotones != null) {
-            animacionBoton(llBotones);
+        if (botonEmpezar != null) {
+           animacionRebote(botonEmpezar, true);
         } else {
-            Log.e("ActividadIntroduccion", "llBotones es null.");
+            Log.e("ActividadIntroduccion", "llBotones es null");
         }
+
+        botonEmpezar.setOnClickListener(v -> animacionExpandirInicio(linearBotones, botonEmpezar));
 
     }
 
@@ -62,7 +67,7 @@ public class ActividadIntroduccion extends AppCompatActivity {
                     .scaleX(1f)
                     .scaleY(1f)
                     .alpha(1f)
-                    .setDuration(1000)
+                    .setDuration(600)
                     .setInterpolator(new DecelerateInterpolator())
                     .start();
         } catch (NullPointerException e) {
@@ -73,8 +78,44 @@ public class ActividadIntroduccion extends AppCompatActivity {
 
     }
 
-    private void animacionBoton(View vista) {
+
+
+    private void animacionRebote(View vista, boolean animacionRebote) {
+        vista.setVisibility(View.VISIBLE);
+        vista.setScaleX(0f);
+        vista.setScaleY(0f);
+        vista.setAlpha(0f);
+
+        vista.animate()
+                .scaleX(1f)
+                .scaleY(1f)
+                .alpha(1f)
+                .setDuration(500)
+                .setStartDelay(500)
+                .setInterpolator(new DecelerateInterpolator())
+                .withEndAction(() -> {
+                    if (animacionRebote) {
+                        iniciarReboteBoton(vista);
+                    }
+                })
+                .start();
+    }
+
+
+    private void iniciarReboteBoton(View boton) {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(boton, "translationY", 0, -15, 0);
+        animator.setDuration(800);
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setRepeatMode(ValueAnimator.RESTART);
+        animator.start();
+    }
+
+
+    private void animacionExpandirInicio(View vista, MaterialButton botonEmpezar) {
         try {
+            //Hacer invisible el boton de empezar una vez clicado e iniciar animación
+            botonEmpezar.setVisibility((View.GONE));
             vista.setVisibility(View.VISIBLE);
             vista.setTranslationY(200f);
             vista.setAlpha(0f);
@@ -82,8 +123,7 @@ public class ActividadIntroduccion extends AppCompatActivity {
             vista.animate()
                     .translationY(0)
                     .alpha(1f)
-                    .setDuration(1000)
-                    .setStartDelay(1000)
+                    .setDuration(600)
                     .start();
         } catch (NullPointerException e) {
             Log.e("ActividadIntroduccion", "Error al animar los botones: La vista es null", e);
@@ -91,5 +131,7 @@ public class ActividadIntroduccion extends AppCompatActivity {
             Log.e("ActividadIntroduccion", "Error en la animación de los botones: Parámetro inválido", e);
         }
     }
+
+
 
 }
--- app/src/main/java/com/example/carnowapp/vista/activity/SplashScreenActivity.java
+++ app/src/main/java/com/example/carnowapp/vista/activity/SplashScreenActivity.java
@@ -1,11 +1,11 @@
-package com.example.carnowapp.vista;
+package com.example.carnowapp.vista.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
 
-public class ActividadSplashScreen extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
 
     @Override
@@ -16,7 +16,7 @@ public class ActividadSplashScreen extends AppCompatActivity {
         //Mantener la pantalla activa mientras se muestra el SplashScreen
         splashScreen.setKeepOnScreenCondition(() -> false);
         //Enviar a la actividad de introducción
-        startActivity(new Intent(this, ActividadIntroduccion.class));
+        startActivity(new Intent(this, LoginActivity.class));
         finish();
 
 
